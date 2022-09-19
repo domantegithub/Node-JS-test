baddb583c143f3ce42cae249f5efb815de8d262c
domantegithub <domante.nav@gmail.com>
2022-09-20 00:00:48 +0300

    fetch api

--- 1TEST/src/index.js
+++ 1TEST/src/index.js
@@ -4,27 +4,28 @@ const mysql = require("mysql2/promise");
 const fetch = require("node-fetch");
 
 const { port, dbconfig } = require("./config");
+const { response } = require("express");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+const URL = "https://jsonplaceholder.typicode.com/users";
 
 app.get('/', async (req, res) => {
     try {
-        const randomUsers = await fetch("https://jsonplaceholder.typicode.com/users", {
-          method:"GET"
-        });
-        console.log(randomUsers);
-    const randomUsersResponse = await randomUsers.json();
-    const userName = randomUsersResponse.name;
-    console.log(userName);
-      const con = await mysql.createConnection(dbconfig);
-      const [response] = await con.execute(`INSERT INTO users (name) VALUES (${mysql.escape(userName)}) `);
-    //   const [response] = await con.execute(`SELECT * FROM users`);
-      await con.end();
-      res.send(response);
+        const getData = await fetch(URL);
+        const data = await getData.json();
+    const userName = data[0].name;
+    const userEmail=data[0].email;
+    const userAdress = data[0].address.street;
+    const con = await mysql.createConnection(dbconfig);
+    await con.execute(`INSERT INTO users (name, email, address) VALUES (${mysql.escape(userName)}, ${mysql.escape(userEmail)}, ${mysql.escape(userAdress)}) `);
+    const [response] = await con.execute(`SELECT * FROM users`);
+    await con.end();
+    console.log(userName, userEmail);
+    res.send(response);
     } catch (e) {
       res.status(400).send({ error: 'Error' });
     }
